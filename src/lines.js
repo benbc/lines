d158a24504fde82fa8517a8c373b8c8cc97a7f94
Ben Butler-Cole <ben@bridesmere.com>
2025-03-20 21:32:49 +0000

    Navigation between scenes

--- src/lines.js
+++ src/lines.js
@@ -5,34 +5,87 @@ window.addEventListener("keydown", function (e) {
 });
 
 document.addEventListener("keydown", function (event) {
-  if (event.key === "ArrowRight") {
-    selectNextLine();
-  } else if (event.key === "ArrowLeft") {
-    selectPreviousLine();
-  } else if (event.key === "ArrowDown") {
-    toggleLineDisplay();
+  if (event.ctrlKey) {
+    if (event.key === "ArrowRight") {
+      nextScene();
+    } else if (event.key === "ArrowLeft") {
+      previousScene();
+    }
+  } else {
+    if (event.key === "ArrowRight") {
+      selectNextLine();
+    } else if (event.key === "ArrowLeft") {
+      selectPreviousLine();
+    } else if (event.key === "ArrowDown") {
+      toggleLineDisplay();
+    }
   }
 });
 
-function selectLine(dirFn) {
+function nextScene() {
+  return moveScene(findNext);
+}
+
+function previousScene() {
+  return moveScene(findPrevious);
+}
+
+function moveScene(dir) {
   const currentLine = document.querySelector("p.current-line");
-  const siblingLine = dirFn(currentLine);
-  if (!siblingLine || siblingLine.tagName !== "P") return;
-  currentLine.classList.remove("current-line");
-  currentLine.classList.remove("display");
-  siblingLine.classList.add("current-line");
-  siblingLine.scrollIntoView({
-    behavior: "smooth",
-    block: "center",
-  });
+  currentHeading = findPrevious(currentLine, "H1");
+
+  heading = dir(currentHeading, "H1");
+  if (!heading) return;
+
+  firstLine = findNext(heading, "P");
+  deselectLine(currentLine);
+  selectLine(firstLine);
+}
+
+function findNext(from, tag) {
+  next = from;
+  while ((next = next.nextElementSibling)) {
+    if (next.tagName === tag) {
+      return next;
+    }
+  }
+}
+function findPrevious(from, tag) {
+  next = from;
+  while ((next = next.previousElementSibling)) {
+    if (next.tagName === tag) {
+      return next;
+    }
+  }
 }
 
 function selectNextLine() {
-  selectLine((line) => line.nextElementSibling);
+  moveSelected((line) => line.nextElementSibling);
 }
 
 function selectPreviousLine() {
-  selectLine((line) => line.previousElementSibling);
+  moveSelected((line) => line.previousElementSibling);
+}
+
+function moveSelected(dirFn) {
+  const currentLine = document.querySelector("p.current-line");
+  const siblingLine = dirFn(currentLine);
+  if (!siblingLine || siblingLine.tagName !== "P") return;
+  deselectLine(currentLine);
+  selectLine(siblingLine);
+}
+
+function deselectLine(line) {
+  line.classList.remove("current-line");
+  line.classList.remove("display");
+}
+
+function selectLine(line) {
+  line.classList.add("current-line");
+  line.scrollIntoView({
+    behavior: "smooth",
+    block: "center",
+  });
 }
 
 function toggleLineDisplay() {
