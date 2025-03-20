eff89c9cb5983f6d877f18eb4eec52748603874e
Ben Butler-Cole <ben@bridesmere.com>
2025-03-20 20:49:38 +0000

    Only show initial letters of words, toggled with up/down keys

--- src/lines.js
+++ src/lines.js
@@ -1,8 +1,18 @@
+window.addEventListener("keydown", function (e) {
+  if (["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"].includes(e.key)) {
+    e.preventDefault();
+  }
+});
+
 document.addEventListener("keydown", function (event) {
   if (event.key === "ArrowRight") {
     selectNextLine();
   } else if (event.key === "ArrowLeft") {
     selectPreviousLine();
+  } else if (event.key === "ArrowDown") {
+    displayCurrentLine();
+  } else if (event.key === "ArrowUp") {
+    screenCurrentLine();
   }
 });
 
@@ -20,4 +30,14 @@ function selectNextLine() {
 
 function selectPreviousLine() {
   selectLine((line) => line.previousElementSibling);
+}
+
+function displayCurrentLine() {
+  const currentLine = document.querySelector("p.current-line");
+  currentLine.classList.add("display");
+}
+
+function screenCurrentLine() {
+  const currentLine = document.querySelector("p.current-line");
+  currentLine.classList.remove("display");
 }
