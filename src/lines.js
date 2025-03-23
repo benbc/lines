a16cb6c3545b4d43b67dc7a8e8007009f7730332
Ben Butler-Cole <ben@bridesmere.com>
2025-03-23 14:08:17 +0000

    Consistently and correctly mark all vars and consts

--- src/lines.js
+++ src/lines.js
@@ -2,7 +2,7 @@ window.addEventListener("load", (_) => control());
 
 async function control() {
   while (true) {
-    var event = await keyPress("ArrowLeft", "ArrowRight", "ArrowDown", "l");
+    const event = await keyPress("ArrowLeft", "ArrowRight", "ArrowDown", "l");
     switch (event.key) {
       case "ArrowLeft":
         if (event.ctrlKey) previousScene();
@@ -23,18 +23,18 @@ async function control() {
 }
 
 async function learnLine() {
-  var chunkSize = 3;
-  var linesAbove = Math.min(chunkSize - 1, countLinesAbove());
-  var linesBelow = Math.min(chunkSize - 1, countLinesBelow());
+  const chunkSize = 3;
+  const linesAbove = Math.min(chunkSize - 1, countLinesAbove());
+  const linesBelow = Math.min(chunkSize - 1, countLinesBelow());
   moveBack(linesAbove);
   for (var i = 0; i < linesAbove + 1 + linesBelow; i++) {
-    learnChunk(chunkSize);
+    await learnChunk(chunkSize);
     moveForward(1);
   }
 }
 
 async function learnChunk(fullChunkSize) {
-  chunkSize = Math.min(fullChunkSize, countLinesAbove() + 1);
+  const chunkSize = Math.min(fullChunkSize, countLinesAbove() + 1);
   for (var fragmentSize = 1; fragmentSize <= chunkSize; fragmentSize++) {
     moveBack(fragmentSize - 1);
     await learnFragment(fragmentSize);
@@ -91,18 +91,18 @@ function previousScene() {
 
 function moveScene(dir) {
   const currentLine = getCurrentLine();
-  currentHeading = findPrevious(currentLine, "H1");
+  const currentHeading = findPrevious(currentLine, "H1");
 
-  heading = dir(currentHeading, "H1");
+  const heading = dir(currentHeading, "H1");
   if (!heading) return;
 
-  firstLine = findNext(heading, "P");
+  const firstLine = findNext(heading, "P");
   deselectLine(currentLine);
   selectLine(firstLine);
 }
 
 function findNext(from, tag) {
-  next = from;
+  var next = from;
   while ((next = next.nextElementSibling)) {
     if (next.tagName === tag) {
       return next;
@@ -111,7 +111,7 @@ function findNext(from, tag) {
 }
 
 function findPrevious(from, tag) {
-  next = from;
+  var next = from;
   while ((next = next.previousElementSibling)) {
     if (next.tagName === tag) {
       return next;
